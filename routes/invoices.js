2eb619404201ae974233a1ad5f1df1c31ea5f19c
Omnicrafter <chen.jeffrey.a@gmail.com>
2023-12-25 23:36:40 -0600

    Add routes for invoices CRUD operations

--- routes/invoices.js
+++ routes/invoices.js
@@ -2,21 +2,91 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-module.exports = router;
-
 router.get("/", async (req, res, next) => {
   try {
-    const { id } = req.query;
-    let result;
-
-    if (id) {
-      result = await db.query(`SELECT * FROM invoices WHERE id = $1`, [id]);
-      return res.json({ invoice: result.rows[0] });
-    } else {
-      result = await db.query(`SELECT * FROM invoices`);
-      return res.json({ invoices: result.rows });
-    }
+    const result = await db.query("SELECT * FROM invoices");
+    return res.json({ invoices: result.rows });
   } catch (e) {
     return next(e);
   }
 });
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await db.query(
+      `SELECT invoices.id, invoices.amt, invoices.paid, invoices.add_date, invoices.paid_date, 
+      companies.code, companies.name, companies.description 
+      FROM invoices 
+      INNER JOIN companies ON invoices.comp_code = companies.code 
+      WHERE invoices.id = $1`,
+      [id]
+    );
+    if (result.rows.length === 0) {
+      throw new ExpressError("Invoice not found", 404);
+    }
+    const data = result.rows[0];
+    const response = {
+      invoice: {
+        id: data.id,
+        amt: data.amt,
+        paid: data.paid,
+        add_date: data.add_date,
+        paid_date: data.paid_date,
+        company: {
+          code: data.code,
+          name: data.name,
+          description: data.description,
+        },
+      },
+    };
+    return res.json(response);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { comp_code, amt } = req.body;
+    const result = await db.query(
+      "INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING *",
+      [comp_code, amt]
+    );
+    return res.status(201).json({ invoice: result.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { amt } = req.body;
+    const result = await db.query(
+      "UPDATE invoices SET amt = $1 WHERE id = $2 RETURNING *",
+      [amt, id]
+    );
+    if (result.rows.length === 0) {
+      throw new ExpressError("Invoice not found", 404);
+    }
+    return res.json({ invoice: result.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await db.query("DELETE FROM invoices WHERE id = $1", [id]);
+    if (result.rowCount === 0) {
+      throw new ExpressError("Invoice not found", 404);
+    }
+    return res.json({ status: "deleted" });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+module.exports = router;
